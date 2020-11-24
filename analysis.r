709e1a11d070c2b6b30cfe9c8656e637c150e4c0
heinrichdk1987 <74563343+heinrichdk1987@users.noreply.github.com>
2020-11-24 21:08:08 +0200

    Add files via upload

---
+++ analysis.r
@@ -0,0 +1,15 @@
+library(tidyverse)
+load("rda/murders.rda")
+
+murders %>% mutate(abb = reorder(abb,rate)) %>%
+  ggplot(aes(abb, rate))+
+  geom_bar(width =0.5 , stat = "identity",color = "black")+
+  coord_flip()
+ggsave("figs/barplot.png")
+
+#Home directory = ~
+#Current directory = .
+#Parent directory = ..
+#Root directory = /
+
+man less
